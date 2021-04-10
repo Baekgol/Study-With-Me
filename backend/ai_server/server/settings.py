be7ab778463a15cfea4b658ff7fe18f6f3e90ad1
chL <37137577+Baekgol@users.noreply.github.com>
2021-04-10 12:06:21 +0900

    Update settings.py

--- backend/ai_server/server/settings.py
+++ backend/ai_server/server/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f2@d9b1-x9z=e%27h6^4vcwrd9^wb$6214@@rl$3uil1i*n9le'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
